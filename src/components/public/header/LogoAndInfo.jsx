5e7e34c050f4c6ef5130cbdb07075a2120f90cc2
savez2223 <mosavez0786@gmail.com>
2024-11-03 21:35:40 +0530

    Navbar

--- src/components/public/header/LogoAndInfo.jsx
+++ src/components/public/header/LogoAndInfo.jsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 const LogoAndInfo = () => {
   return (
     <div className="w-full py-2 px-4">
-      <div className="flex justify-between max-w-screen-xl items-center mx-auto w-full">
-        <Link href={'/'}>
+      <div className="flex justify-between items-center max-w-screen-xl mx-auto w-full">
+        <Link href={"/"}>
           <Image
             src={"/MEC Logo.jpeg"}
             alt="Mewat Engineering College"
@@ -14,8 +14,18 @@ const LogoAndInfo = () => {
             height={40}
           />
         </Link>
+
+        {/* Responsive and closer text block */}
+        <div className="ml-4 flex-1 text-left">
+          <h1 className="text-2xl md:text-5xl font-bold">
+            Mewat Engineering College
+          </h1>
+          <p className="text-lg md:text-2xl font-medium">Haryana Wakf Board</p>
+        </div>
+
+        {/* Button */}
         <div className="hidden md:block">
-          <Button href={'#'} text={'Log In'}/>
+          <Button href={"#"} text={"Log In"} />
         </div>
       </div>
     </div>
